193f09ac9236d744d4d7fe9a7e556b76135d4ad0
edgararroyo30 <edgararroyo306@gmail.com>
2024-08-10 14:01:45 -0600

    Fix issues accesing keys in .json file

--- Version 1.2.5 - TK & CTK/protect/key.py
+++ Version 1.2.5 - TK & CTK/protect/key.py
@@ -6,29 +6,29 @@ class Keys():
     def __init__(self):
         pass
 
-    def public_table_key(self):
-        with open("keys.json", "r") as file:
+    def public_table_key():
+        with open("C:/Users/edgar/OneDrive/Escritorio/Current Projects/Password Manager/Version 1.2.5 - TK & CTK/keys/keys.json", "r") as file:
             config = json.load(file)
         key = config["PUBLIC_TABLE_KEY"]
 
         return key
 
-    def private_db_key(self):
-        with open("keys.json", "r") as file:
+    def private_db_key():
+        with open("C:/Users/edgar/OneDrive/Escritorio/Current Projects/Password Manager/Version 1.2.5 - TK & CTK/keys/keys.json", "r") as file:
             config = json.load(file)
         key = config["PRIVATE_DB_KEY"]
 
         return key
 
-    def public_db_key(self):
-        with open("keys.json", "r") as file:
+    def public_db_key():
+        with open("C:/Users/edgar/OneDrive/Escritorio/Current Projects/Password Manager/Version 1.2.5 - TK & CTK/keys/keys.json", "r") as file:
             config = json.load(file)
         key = config["PUBLIC_DB_KEY"]
 
         return key
 
-    def private_table_key(self):
-        with open("keys.json", "r") as file:
+    def private_table_key():
+        with open("C:/Users/edgar/OneDrive/Escritorio/Current Projects/Password Manager/Version 1.2.5 - TK & CTK/keys/keys.json", "r") as file:
             config = json.load(file)
         key = config["PRIVATE_TABLE_KEY"]
 
