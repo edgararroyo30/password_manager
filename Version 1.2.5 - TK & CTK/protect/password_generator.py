78d517aa9106dd02e07699574c16e1b41656c939
edgararroyo30 <edgararroyo306@gmail.com>
2024-08-10 22:02:32 -0600

    Renaming directories changong 'version' to 'v'

--- Version 1.2.5 - TK & CTK/protect/password_generator.py
+++
@@ -1,9 +0,0 @@
-import secrets
-
-def generate_password():
-    """
-    Creates a save password
-    """
-    password_lenght = 12
-    password = secrets.token_urlsafe(password_lenght)
-    return password
