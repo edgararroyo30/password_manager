3218a0b9ba58bd3fd87299923743be406394a256
edgararroyo30 <edgararroyo306@gmail.com>
2024-08-11 14:21:27 -0600

    Function to create and locate the popup menu created, as well with functions to delete, edit and copy selected data

--- v1.2.5 - TK & CTK/client/gui.py
+++ v1.2.5 - TK & CTK/client/gui.py
@@ -1,12 +1,13 @@
 """
 All the sections in the menu 
 """
+import pyperclip
 import tkinter as tk
 from tkinter import ttk
 import customtkinter as ctk
 from ttkthemes import ThemedStyle
 from client.frame_builder import FrameBuilder
-from model.admin_dao import cuenta_id, listar_usuarios, save, user
+from model.admin_dao import cuenta_id, listar_usuarios, save, user, eliminar
 from protect.password_generator import generate_password
 
 
@@ -62,37 +63,56 @@ class Gui():
         self.tabla.column("#0", width=0, stretch=False)
         self.tabla.config(displaycolumns=(
             'Web Site', 'Username', 'Password'))
-        self.update_table()
+        self.update_table('')
 
-    def update_table(self):
+    def update_table(self, status):
         children = self.tabla.get_children()
         
-
-        if not children:
+        if status == "user_deleted":
 
             try:
+                for item in self.tabla.get_children():
+                    self.tabla.delete(item)
+
                 for p in listar_usuarios(0):
 
-                    self.tabla.insert('', 0,iid=p[0],
-                       values=(p[1], p[2], p[3]))
+
+                    self.tabla.insert('', 0,iid=p[0], text=p[0],
+                           values=(p[1], p[2], p[3]))
             except TypeError as e:
                 if "'NoneType' object is not iterable" in str(e):
-                    print("Error: Tried to iterate on a None object.")
+                        print("Error: Tried to iterate on a None object.")
                 else:
-                    print(f"Unexpected error: {e}")
+                        print(f"Unexpected error: {e}")
+
         else:
-            try:
-                last_item = self.tabla.get_children()[0]
 
-                for p in listar_usuarios(int(last_item)):
+            if not children:
 
-                    self.tabla.insert('', 0, iid=p[0],
-                       values=(p[1], p[2], p[3]))
-            except TypeError as e:
-                if "'NoneType' object is not iterable" in str(e):
-                    print("Error: Tried to iterate on a None object.")
-                else:
-                    print(f"Unexpected error: {e}")
+                try:
+                    for p in listar_usuarios(0):
+
+                        self.tabla.insert('', 0,iid=p[0], text=p[0],
+                           values=(p[1], p[2], p[3]))
+                except TypeError as e:
+                    if "'NoneType' object is not iterable" in str(e):
+                        print("Error: Tried to iterate on a None object.")
+                    else:
+                        print(f"Unexpected error: {e}")
+            else:
+                try:
+                    last_item = self.tabla.get_children()[0]
+                    
+
+                    for p in listar_usuarios(int(last_item)):
+
+                        self.tabla.insert('', 0, iid=p[0], text=p[0],
+                           values=(p[1], p[2], p[3]))
+                except TypeError as e:
+                    if "'NoneType' object is not iterable" in str(e):
+                        print("Error: Tried to iterate on a None object.")
+                    else:
+                        print(f"Unexpected error: {e}") 
 
     def content(self):
         """
@@ -133,6 +153,8 @@ class Gui():
 
         self.styles()
 
+        self.pop_menu()
+
     def update_users_count(self):
         user_count = (f"{cuenta_id()} saved passwords")
         self.label_usuarios.config(
@@ -169,7 +191,7 @@ class Gui():
                self.mi_contrasena_add.get())
             save(user_data)
             new_user_window.destroy()
-            self.update_table()
+            self.update_table('')
             self.update_users_count()
 
         def destroy():
@@ -254,12 +276,10 @@ class Gui():
 
         new_user_content()
 
-
-
-
     def search_and_highlight(self, tree, search_string):
         
         if search_string == "":
+            
             for item in self.detached_items:
                 tree.reattach(item,'','end')
         else:
@@ -273,6 +293,148 @@ class Gui():
 
                 else:
                     self.detached_items.append(item)
+                    
                     tree.detach(item)
                 
+    def edit_user(self):
+        """
+        Window for new users
+        """
+        new_user_window = tk.Toplevel(self.frame)
+        new_user_window.config(background=self.style.lookup('TFrame', 'background'))
+        new_user_window.title('Edit User')
+        new_user_window.iconbitmap('image/app-icon.ico')
+        style = ThemedStyle(new_user_window)
 
+        new_user_window.geometry("310x250")
+
+        x = self.frame.winfo_x() + (self.frame.winfo_width() // 2) - \
+            (new_user_window.winfo_width() // 2)
+        y = self.frame.winfo_y() + (self.frame.winfo_height() // 2) - \
+            (new_user_window.winfo_height() // 2)
+
+        new_user_window.geometry("+{}+{}".format(x, y))
+
+        new_user_window.lift()
+
+        new_user_window.overrideredirect(False)
+
+        def save_data():
+            
+            user_data = user(
+                self.mi_sitio_web_edit.get(),
+                self.mi_nombre_usuario_edit.get(),
+               self.mi_contrasena_edit.get())
+            save(user_data)
+            new_user_window.destroy()
+            self.update_table()
+            self.update_users_count()
+
+        def destroy():
+            new_user_window.destroy()
+
+        def edit_user_content():
+
+            label_sitio_web = ttk.Label(new_user_window, text='Web site')
+            style.configure(style='Equilux.TLabel')
+            label_sitio_web.config(
+            width=17, font=('Segoe UI', 10))
+            label_sitio_web.grid(row=0, padx=(1, 10), pady=(14, 4))
+
+            label_nombre_usuario = ttk.Label(
+            new_user_window, text='Username ')
+            style.configure(style='Equilux.TLabel')
+            label_nombre_usuario.config(
+            width=17, font=('Segoe UI', 10))
+            label_nombre_usuario.grid(row=2, padx=(1, 10), pady=(14, 4))
+
+            label_contraseña = ttk.Label(new_user_window, text='Password ')
+            style.configure(style='Equilux.TLabel')
+            label_contraseña.config(
+            width=17, font=('Segoe UI', 10))
+            label_contraseña.grid(row=4, padx=(1, 10), pady=(14, 4))
+
+        
+            self.mi_sitio_web_edit = tk.StringVar()
+
+            entry_sitio_web = ttk.Entry(
+            new_user_window, textvariable=self.mi_sitio_web_edit)
+            style.configure(style='Equilux.TEntry')
+            entry_sitio_web.config(width=30, font=('Segoe UI', 12))
+            entry_sitio_web.grid(
+            row=1, padx=(15, 16), columnspan=3)
+
+            self.mi_nombre_usuario_edit = tk.StringVar()
+
+            entry_nombre_usuario = ttk.Entry(
+            new_user_window, textvariable=self.mi_nombre_usuario_edit)
+            style.configure(style='Equilux.TEntry')
+            entry_nombre_usuario.config(width=30,
+                                    font=('Segoe UI', 12))
+            entry_nombre_usuario.grid(
+            row=3, padx=(16, 16), columnspan=3)
+
+            mi_contrasena = generate_password()
+            self.mi_contrasena_edit = tk.StringVar()
+
+            entry_contrasena = ttk.Entry(
+            new_user_window, textvariable=self.mi_contrasena_edit)
+            entry_contrasena.insert(0, mi_contrasena)
+            style.configure(
+            style='Equilux.TEntry')
+
+            entry_contrasena.config(width=30,
+                                font=('Segoe UI', 12))
+            entry_contrasena.grid(
+            row=5, padx=(16, 16), columnspan=3)
+
+            boton_guardar = ttk.Button(
+            new_user_window, text='Save', command=save_data)
+
+            style.configure("RoundedButton.TButton", width=25, font=('Segoe UI', 12, 'bold'),
+                        cursor='hand2', focuscolor='none', borderwidth=2,
+                        bd=2, relief="solid", anchor='center',
+                        highlightcolor='#35BD6F', highlightbackground='#35BD6F')
+
+            boton_guardar.grid(
+            row=6, column=0,  padx=(30, 10), pady=(14, 10))
+
+            boton_cancelar = ttk.Button(
+            new_user_window, text='Cancel', command=destroy)
+
+            style.configure("RoundedButton.TButton", width=25, font=('Segoe UI', 12, 'bold'),
+                        cursor='hand2', focuscolor='none', borderwidth=2,
+                        bd=2, relief="solid", anchor='center',
+                        highlightcolor='#35BD6F', highlightbackground='#35BD6F')
+
+            boton_cancelar.grid(
+            row=6, column=1,  padx=(0, 20), pady=(14, 10))
+
+        edit_user_content()   
+
+    def pop_menu(self):
+        def show_menu (event):
+            self.popup_menu.post(event.x_root, event.y_root)
+        def copy_data():
+            password = self.tabla.item(
+                self.tabla.selection())['values'][2]
+            pyperclip.copy(password)
+        def delete_data():
+            user_id = self.tabla.item(self.tabla.selection())["text"]
+            eliminar(user_id)
+            self.update_users_count()
+            self.update_table("user_deleted")
+
+        self.popup_menu = tk.Menu(
+            self.frame, type='normal', bg='#373737', foreground='#D3D3D3', tearoff=0)
+        self.popup_menu.config(relief='sunken',activebackground='#555',
+                               activeforeground='#D3D3D3', bd=0, activeborderwidth=0)
+
+        self.popup_menu.add_command(label="Edit", command=self.edit_user)
+        self.popup_menu.add_command(
+            label="Delete", command = delete_data)
+        self.popup_menu.add_separator()
+        self.popup_menu.add_command(
+            label="Copy", command=copy_data)
+
+        self.tabla.bind("<Button-3>", show_menu)
