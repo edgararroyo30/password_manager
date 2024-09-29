ec169ec3f0b66fde21563cf708dd5d85b03f464c
edgararroyo30 <edgararroyo306@gmail.com>
2024-09-28 19:50:41 -0600

    New methods to generate the keys if are non existent and to store them using Windows Credencial Manager

---
+++ v1.2.5 - TK & CTK/protect/key_generation.py
@@ -0,0 +1,62 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+import keyring
+import base64
+
+def create_keys():
+
+    private_key = rsa.generate_private_key(
+                public_exponent=65537,
+                key_size=2048,
+                backend=default_backend)
+    public_key = private_key.public_key()
+
+    private_key_pem = private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.PKCS8,
+                encryption_algorithm=serialization.NoEncryption()
+                )
+
+    public_key_pem = public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+    return private_key_pem.decode('utf-8'), public_key_pem.decode('utf-8')
+
+def split_key(data, chunk_size):
+    return [data[i:i+chunk_size] for i in range(0, len(data), chunk_size)]
+
+
+def store_large_encryption_key(service_name, key_name, key_data, chunk_size=500):
+    key_parts = split_key(key_data.encode('utf-8'), chunk_size)
+
+    for i, part in enumerate(key_parts):
+        keyring.set_password(service_name, f'{key_name}_part_{i}', base64.b64encode(part).decode('utf-8'))
+
+    print(f"{len(key_parts)} parts of the {key_name} key have been securely stored in Windows Credential Manager.")
+
+def check_keys_exist(service_name):
+    private_key = keyring.get_password(service_name, 'private_key')
+    public_key = keyring.get_password(service_name, 'public_key')
+
+    if private_key and public_key:
+        return True
+    else:
+        return False
+
+def store_encryption_keys(service_name, private_key, public_key):
+
+    if not check_keys_exist(service_name):
+        store_large_encryption_key (service_name, "private_key", private_key)
+        keyring.set_password(service_name, 'public_key', public_key)
+        print("Keys have been securely stored.")
+    else: 
+        print("Keys already exist. Skipping storage.")
+
+private_key_passwords, public_key_passwords = create_keys()
+
+store_encryption_keys("Password_keys", private_key_passwords, public_key_passwords)
+
+
+
