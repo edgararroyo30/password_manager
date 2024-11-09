afe59d5512b5d2d3955a7e29c83f093a044db4b6
edgararroyo30 <edgararroyo306@gmail.com>
2024-11-09 08:11:33 -0600

    Fixing typos

--- v1.2.5 - TK & CTK/main.py
+++ v1.2.5 - TK & CTK/main.py
@@ -4,6 +4,7 @@ Execute the mainloop if file name is main
 
 from client.app import App
 from model.admin_dao import create_table, create_table_codigo
+from protect.key_generation import KeyGeneration
 
 def main():
     """
@@ -12,8 +13,11 @@ def main():
     app = App()
     app.mainloop()
 
-
+def create_keys():
+    generator = KeyGeneration()
+    
 if __name__ == '__main__':
     create_table()
     create_table_codigo()
+    create_keys()
     main()
--- v1.2.5 - TK & CTK/protect/key_generation.py
+++ v1.2.5 - TK & CTK/protect/key_generation.py
@@ -4,59 +4,66 @@ from cryptography.hazmat.primitives import serialization
 import keyring
 import base64
 
-def create_keys():
+class KeyGeneration():
 
-    private_key = rsa.generate_private_key(
-                public_exponent=65537,
-                key_size=2048,
-                backend=default_backend)
-    public_key = private_key.public_key()
+    def __init__(self):
+        self.store()
 
-    private_key_pem = private_key.private_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PrivateFormat.PKCS8,
-                encryption_algorithm=serialization.NoEncryption()
-                )
+    def create_keys(self):
 
-    public_key_pem = public_key.public_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PublicFormat.SubjectPublicKeyInfo)
+        private_key = rsa.generate_private_key(
+                    public_exponent=65537,
+                    key_size=2048,
+                    backend=default_backend)
+        public_key = private_key.public_key()
 
-    return private_key_pem.decode('utf-8'), public_key_pem.decode('utf-8')
+        private_key_pem = private_key.private_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PrivateFormat.PKCS8,
+                    encryption_algorithm=serialization.NoEncryption()
+                    )
 
-def split_key(data, chunk_size):
-    return [data[i:i+chunk_size] for i in range(0, len(data), chunk_size)]
+        public_key_pem = public_key.public_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo)
 
+        return private_key_pem.decode('utf-8'), public_key_pem.decode('utf-8')
 
-def store_large_encryption_key(service_name, key_name, key_data, chunk_size=500):
-    key_parts = split_key(key_data.encode('utf-8'), chunk_size)
+    def split_key(self, data, chunk_size):
+        return [data[i:i+chunk_size] for i in range(0, len(data), chunk_size)]
 
-    for i, part in enumerate(key_parts):
-        keyring.set_password(service_name, f'{key_name}_part_{i}', base64.b64encode(part).decode('utf-8'))
+    def store_large_encryption_key(self, service_name, key_name, key_data, chunk_size=500):
+        key_parts = self.split_key(key_data.encode('utf-8'), chunk_size)
 
-    print(f"{len(key_parts)} parts of the {key_name} key have been securely stored in Windows Credential Manager.")
+        for i, part in enumerate(key_parts):
+            keyring.set_password(service_name, f'{key_name}_part_{i}', base64.b64encode(part).decode('utf-8'))
 
-def check_keys_exist(service_name):
-    private_key = keyring.get_password(service_name, 'private_key')
-    public_key = keyring.get_password(service_name, 'public_key')
+        print(f"{len(key_parts)} parts of the {key_name} key have been securely stored in Windows Credential Manager.")
 
-    if private_key and public_key:
-        return True
-    else:
-        return False
+    def check_keys_exist(self,service_name):
+        private_key = keyring.get_password(service_name, "private_key")
+        public_key = keyring.get_password(service_name, "public_key")
 
-def store_encryption_keys(service_name, private_key, public_key):
+        if private_key and public_key:
+            return True
+        else:
+            return False
 
-    if not check_keys_exist(service_name):
-        store_large_encryption_key (service_name, "private_key", private_key)
-        keyring.set_password(service_name, 'public_key', public_key)
-        print("Keys have been securely stored.")
-    else: 
-        print("Keys already exist. Skipping storage.")
+    def store_encryption_keys(self,service_name, private_key, public_key):
 
-private_key_passwords, public_key_passwords = create_keys()
+        if not self.check_keys_exist(service_name):
+            self.store_large_encryption_key (service_name, "private_key", private_key)
+            keyring.set_password(service_name, "public_key", public_key)
+            print("Keys have been securely stored.")
+            print(keyring.get_password(service_name, "private_key"))
+        else: 
+            print("Keys already exist. Skipping storage.")
 
-store_encryption_keys("Password_keys", private_key_passwords, public_key_passwords)
+    def store(self):
+
+        private_key_passwords, public_key_passwords = self.create_keys()
+
+        self.store_encryption_keys("Password_keys", private_key_passwords, public_key_passwords)
 
 
 
